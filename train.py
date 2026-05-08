b17dd33490cf20b9063a9682b9821f8ae32a1ba1
Osias Noël TOSSOU <osias.tossou@gmail.com>
2026-05-08 05:31:08 +0100

    test

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def main(modelpath, data, outname,epochs,imgsz=640,v=11,p=True):
 
         # ── Résolution & batch ────────────────────────────────────────────────
         imgsz=imgsz,
-        batch=16,  # Fixe — pas auto-batch pour garantir l'équité entre modèles
+        batch=-1,  # Fixe — pas auto-batch pour garantir l'équité entre modèles
 
         # ── Device ────────────────────────────────────────────────────────────
         device=0 if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu',
