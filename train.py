bcc128db0a6e3b935167806dc69905df2dff5a0f
Osias Noël TOSSOU <osias.tossou@gmail.com>
2026-06-03 01:57:02 +0100

    t

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def main(modelpath, data, outname, epochs, imgsz=640, v=11, p=True):
 
         # ── Résolution & batch ────────────────────────────────────────────────
         imgsz=imgsz,
-        batch=16 ,  # Fixe — pas auto-batch pour garantir l'équité entre modèles
+        batch=8 ,  # Fixe — pas auto-batch pour garantir l'équité entre modèles
 
         # ── Device ────────────────────────────────────────────────────────────
         device=device,
@@ -109,7 +109,6 @@ def main(modelpath, data, outname, epochs, imgsz=640, v=11, p=True):
         resume=True,
         #accumulate=4,
         workers=2,
-        val=False,
 
         name=outname,
     )
